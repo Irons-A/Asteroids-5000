019179a9370f09d5ff25b0e694b1f4d8386b20e8
Irons-A <120574631+Irons-A@users.noreply.github.com>
2025-12-09 11:35:26 +0300

    Creating projectiles and pooling system p7

--- Assets/Game/Scripts/Player/Logic/PlayerWeaponSystem.cs
+++ Assets/Game/Scripts/Player/Logic/PlayerWeaponSystem.cs
@@ -1,3 +1,4 @@
+using Core.Projectiles;
 using Core.Systems.ObjectPools;
 using Cysharp.Threading.Tasks;
 using System;
@@ -11,57 +12,88 @@ namespace Player.Logic
 {
     public class PlayerWeaponSystem : ITickable
     {
-        private GameObject _projectilePrefab;
+        private Projectile _projectilePrefab;
         private Transform[] _firePoints;
         private UniversalObjectPool _objectPool;
 
         private bool _shouldShoot = false;
         private bool _canShoot = true;
         private bool _isShooting = false;
+        private bool _isReloading = false;
         private CancellationTokenSource _shootingCTS;
+        private CancellationTokenSource _reloadCTS;
 
         private float _fireRate;
         private int _maxAmmo;
+        private int _ammoCostPerShot;
+        private bool _hasInfiniteAmmo;
         private int _currentAmmo;
         private int _projectileDamage;
         private int _projectileSpeed;
-        private float _reloadSpeed;
+        private float _reloadLength;
         private int _ammoPerReload;
+        private bool _shouldAutoReloadOnLessThanMaxAmmo;
+        private bool _shouldAutoReloadOnNoAmmo;
+        private bool _shouldDepleteAmmoOnReload;
+        private bool _shouldBlockFireWhileReload;
 
         [Inject]
         private void Construct()
+        {
+
+
+        }
+        public void Initialize()
         {
 
         }
 
         public void Tick()
         {
+            ProcessAbilityToSHoot();
+
             ProcessWillToShoot();
+
+            ProcessAutoReloading();
         }
 
-        public void Initialize()
+        private void ProcessAbilityToSHoot()
         {
+            if (_currentAmmo < _ammoCostPerShot && _hasInfiniteAmmo == false)
+            {
+                _canShoot = false;
+            }
+            else if (_isReloading && _shouldBlockFireWhileReload)
+            {
+                _canShoot = false;
+            }
+            else
+            {
+                _canShoot = true;
+            }
 
-        }
-
-        public void SetShouldShoot(bool value)
-        {
-            _shouldShoot = value;
-        }
-
-        private void ProcessWillToShoot()
-        {
             if (_isShooting)
             {
-                if (!_shouldShoot || !_canShoot)
+                if (_shouldShoot == false || _canShoot == false)
                 {
                     StopShooting();
                 }
             }
+        }
 
-            if (_shouldShoot && _canShoot && !_isShooting)
+        private void ProcessWillToShoot()
+        {
+            if (_shouldShoot && _canShoot && _isShooting == false)
             {
                 StartShooting();
+            }
+        }
+
+        private void ProcessAutoReloading()
+        {
+            if (_hasInfiniteAmmo == false && _currentAmmo <= _maxAmmo && _shouldAutoReloadOnLessThanMaxAmmo)
+            {
+                StartReload();
             }
         }
 
@@ -70,6 +102,7 @@ namespace Player.Logic
             if (_isShooting) return;
 
             _isShooting = true;
+
             _shootingCTS = new CancellationTokenSource();
 
             ShootingLoop(_shootingCTS.Token).Forget();
@@ -87,18 +120,99 @@ namespace Player.Logic
         {
             while (!cancellationToken.IsCancellationRequested && _isShooting)
             {
-                ShootBullet();
+                if (_canShoot)
+                {
+                    ShootProjectile();
+
+                    if (_hasInfiniteAmmo == false)
+                    {
+                        _currentAmmo -= _ammoCostPerShot;
+                    }
+                }
 
                 await UniTask.Delay(TimeSpan.FromSeconds(_fireRate),ignoreTimeScale: false,
                     cancellationToken: cancellationToken);
+
+                if (_currentAmmo <= _ammoCostPerShot)
+                {
+                    if (_shouldAutoReloadOnNoAmmo)
+                    {
+                        StartReload();
+                    }
+
+                    break;
+                }
             }
         }
 
-        private void ShootBullet()
+        private void ShootProjectile()
         {
             if (_projectilePrefab == null || _firePoints == null) return;
 
-            
+            foreach (Transform firepoint in _firePoints)
+            {
+
+            }
+        }
+
+        private async UniTaskVoid ReloadLoop(CancellationToken cancellationToken)
+        {
+            try
+            {
+                _isReloading = true;
+
+                if (_shouldDepleteAmmoOnReload)
+                {
+                    _currentAmmo = 0;
+                }
+
+                Debug.Log("reloading");
+
+                await UniTask.Delay(TimeSpan.FromSeconds(_reloadLength), ignoreTimeScale: false,
+                    cancellationToken: cancellationToken);
+
+                _currentAmmo += _ammoPerReload;
+                _currentAmmo = Math.Min(_currentAmmo, _maxAmmo);
+
+                Debug.Log($"Reloaded, ammo: {_currentAmmo}");
+            }
+            catch (OperationCanceledException)
+            {
+                Debug.Log("ReloadCanceled");
+            }
+            finally
+            {
+                _isReloading = false;
+                _reloadCTS?.Dispose();
+                _reloadCTS = null;
+            }
+        }
+
+        public void SetShouldShoot(bool value)
+        {
+            _shouldShoot = value;
+        }
+
+        public void StartReload()
+        {
+            if (_isReloading || _currentAmmo == _maxAmmo || _hasInfiniteAmmo) return;
+
+            if (_shouldBlockFireWhileReload)
+            {
+                StopShooting();
+            }
+
+            _reloadCTS = new CancellationTokenSource();
+
+            ReloadLoop(_reloadCTS.Token).Forget();
+        }
+
+        public void CancelReload()
+        {
+            _isReloading = false;
+            _reloadCTS?.Cancel();
+            _reloadCTS?.Dispose();
+            _reloadCTS = null;
         }
     }
 }
