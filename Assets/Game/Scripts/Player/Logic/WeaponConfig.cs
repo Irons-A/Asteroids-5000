0fe35780c3cab50e6f26e6ece96f488863b88e1e
Irons-A <120574631+Irons-A@users.noreply.github.com>
2025-12-13 12:09:11 +0300

    Refactoring PlayerWeaponSystem p4

--- Assets/Game/Scripts/Player/Logic/WeaponConfig.cs
+++ Assets/Game/Scripts/Player/Logic/WeaponConfig.cs
@@ -8,24 +8,52 @@ namespace Player.Logic
 {
     public class WeaponConfig
     {
-        public PoolableObjectType ProjectileType { get; set; }
-        public Transform[] FirePoints { get; set; }
-        public float ProjectileSpeed { get; set; }
-        public bool ProjectileDelayedDestruction { get; set; }
-        public float DestroyProjectileAfter { get; set; }
-        public int ProjectileDamage { get; set; }
-        public DamagerAffiliation ProjectileAffiliation { get; set; }
-        public DamagerDurability ProjectileDurability { get; set; }
-        public bool ShouldSetFirepointAsProjectileParent { get; set; }
-        public float FireRateInterval { get; set; }
-        public int MaxAmmo { get; set; }
-        public int AmmoCostPerShot { get; set; }
-        public bool HasInfiniteAmmo { get; set; }
-        public float ReloadLength { get; set; }
-        public int AmmoPerReload { get; set; }
-        public bool ShouldAutoReloadOnLessThanMaxAmmo { get; set; }
-        public bool ShouldAutoReloadOnNoAmmo { get; set; }
-        public bool ShouldDepleteAmmoOnReload { get; set; }
-        public bool ShouldBlockFireWhileReload { get; set; }
+        public PoolableObjectType ProjectileType { get; private set; }
+        public Transform[] FirePoints { get; private set; }
+        public float ProjectileSpeed { get; private set; }
+        public bool ProjectileDelayedDestruction { get; private set; }
+        public float DestroyProjectileAfter { get; private set; }
+        public int ProjectileDamage { get; private set; }
+        public DamagerAffiliation ProjectileAffiliation { get; private set; }
+        public DamagerDurability ProjectileDurability { get; private set; }
+        public bool ShouldSetFirepointAsProjectileParent { get; private set; }
+        public float FireRateInterval { get; private set; }
+        public int MaxAmmo { get; private set; }
+        public int AmmoCostPerShot { get; private set; }
+        public bool HasInfiniteAmmo { get; private set; }
+        public float ReloadLength { get; private set; }
+        public int AmmoPerReload { get; private set; }
+        public bool ShouldAutoReloadOnLessThanMaxAmmo { get; private set; }
+        public bool ShouldAutoReloadOnNoAmmo { get; private set; }
+        public bool ShouldDepleteAmmoOnReload { get; private set; }
+        public bool ShouldBlockFireWhileReload { get; private set; }
+
+        public void Configure(PoolableObjectType projectileType, Transform[] firepoints, float projectileSpeed,
+            bool projectileDelayedDestruction, float destroyProjectileAfter, int projectileDamage,
+            DamagerAffiliation projectileAffiliation, DamagerDurability projectileDurability,
+            bool shouldSetFirepointAsProjectileParent, float fireRateInterval, int maxAmmo, int ammoCostPerShot,
+            bool hasInfiniteAmmo, float reloadLength, int ammoPerReload, bool shouldAutoReloadOnLessThanMaxAmmo,
+            bool shouldAutoReloadOnNoAmmo, bool shouldDepleteAmmoOnReload, bool shouldBlockFireWhileReaload)
+        {
+            ProjectileType = projectileType;
+            FirePoints = firepoints;
+            ProjectileSpeed = projectileSpeed;
+            ProjectileDelayedDestruction = projectileDelayedDestruction;
+            DestroyProjectileAfter = destroyProjectileAfter;
+            ProjectileDamage = projectileDamage;
+            ProjectileAffiliation = projectileAffiliation;
+            ProjectileDurability = projectileDurability;
+            ShouldSetFirepointAsProjectileParent = shouldSetFirepointAsProjectileParent;
+            FireRateInterval = fireRateInterval;
+            MaxAmmo = maxAmmo;
+            AmmoCostPerShot = ammoCostPerShot;
+            HasInfiniteAmmo = hasInfiniteAmmo;
+            ReloadLength = reloadLength;
+            AmmoPerReload = ammoPerReload;
+            ShouldAutoReloadOnLessThanMaxAmmo = shouldAutoReloadOnLessThanMaxAmmo;
+            ShouldAutoReloadOnNoAmmo = shouldAutoReloadOnNoAmmo;
+            ShouldDepleteAmmoOnReload = shouldDepleteAmmoOnReload;
+            ShouldBlockFireWhileReload = shouldBlockFireWhileReaload;
+        }
     }
 }
