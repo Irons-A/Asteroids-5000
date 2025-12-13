98d07241bd2b18676d3f60e4dba9602bdd7a386c
Irons-A <120574631+Irons-A@users.noreply.github.com>
2025-12-13 21:10:37 +0300

    Adding health system p1

---
+++ Assets/Game/Scripts/Core/Systems/HealthSystem.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Core.Systems
+{
+    public class HealthSystem
+    {
+        public event Action<int> OnHealthChanged;
+        public event Action OnHealthDepleted;
+
+        public int MaxHealth { get; private set; }
+        public int CurrentHealth { get; private set; }
+
+        public void Configure(int maxHealth, bool setCurrentHealthToMax, int currentHealth = 1)
+        {
+            MaxHealth = maxHealth;
+            
+            if (setCurrentHealthToMax)
+            {
+                CurrentHealth = MaxHealth;
+            }
+        }
+
+        public void TakeDamage(int damage)
+        {
+            damage = Math.Max(1, damage);
+
+            CurrentHealth -= damage;
+
+            OnHealthChanged?.Invoke(CurrentHealth);
+
+            if (CurrentHealth <= 0)
+            {
+                OnHealthDepleted?.Invoke();
+            }
+        }
+
+        public void RestoreHealth(int health)
+        {
+            health = Math.Min(1, health);
+
+            CurrentHealth += health;
+            CurrentHealth = Math.Min(CurrentHealth, MaxHealth);
+
+            OnHealthChanged?.Invoke(CurrentHealth);
+        }
+    }
+}
