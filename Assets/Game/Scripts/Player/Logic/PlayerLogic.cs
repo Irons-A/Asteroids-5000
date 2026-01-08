dc6a04a7035ebe8354c95a04d2700761b6c6b888
Irons-A <120574631+Irons-A@users.noreply.github.com>
2026-01-08 18:55:24 +0300

    Refactored player logic

--- Assets/Game/Scripts/Player/Logic/PlayerLogic.cs
+++ Assets/Game/Scripts/Player/Logic/PlayerLogic.cs
@@ -16,24 +16,13 @@ using Zenject;
 
 namespace Player.Logic
 {
-    public class PlayerLogic : ITickable, IFixedTickable, IDisposable
+    public class PlayerLogic : ITickable, IDisposable
     {
-        //private const float MinEngineParticlesToggleDelay = 0.1f;
-        
         private readonly PlayerShipSettings _shipSettings;
-        //private readonly PlayerWeaponsSettings _weaponsSettings; 
-        // private readonly UniversalPlayerWeaponSystem _bulletWeaponSystem;
-        // private readonly PlayerWeaponConfig _bulletWeaponConfig;
-        // private readonly UniversalPlayerWeaponSystem _laserWeaponSystem;
-        // private readonly PlayerWeaponConfig _laserWeaponConfig;
-        //private readonly HealthSystem _healthSystem;
-        // private readonly InvulnerabilityLogic _invulnerabilityLogic;
-        // private readonly UncontrollabilityLogic _uncontrollabilityLogic;
         private readonly PlayerHealthLogic _healthLogic;
         private readonly PlayerMovementLogic _movementLogic;
         private readonly PlayerWeaponsLogic _weaponsLogic;
         private readonly CustomPhysics _playerPhysics;
-        
         private readonly PlayerUIModel _playerUIModel;
         private readonly SignalBus _signalBus;
         private readonly ParticleService _particleService;
@@ -43,24 +32,16 @@ namespace Player.Logic
         private CollisionHandler _playerCollisionHandler;
         private SpriteRenderer _playerSpriteRenderer;
         private ParticleSystem _playerEngineParticles;
-
-        //private float _lastParticleToggleTime = 0f;
+        
         private bool _isConfigured = false;
         
-        public PlayerLogic(JsonConfigProvider configProvider, PlayerHealthLogic healthLogic, PlayerMovementLogic movementLogic, 
-            PlayerWeaponsLogic weaponsLogic, CustomPhysics playerPhysics, PlayerUIModel playerUIModel, 
-            SignalBus signalBus, ParticleService particleService)
+        public PlayerLogic(JsonConfigProvider configProvider, PlayerHealthLogic healthLogic,
+            PlayerMovementLogic movementLogic, PlayerWeaponsLogic weaponsLogic, CustomPhysics playerPhysics,
+            PlayerUIModel playerUIModel, SignalBus signalBus, ParticleService particleService)
         {
             _shipSettings = configProvider.PlayerShipSettingsRef;
-            //_weaponsSettings = configProvider.PlayerWeaponsSettingsRef;
 
             _playerPhysics = playerPhysics;
-
-            //_bulletWeaponSystem = bulletWeapon;
-            //_bulletWeaponConfig = bulletWeaponConfig;
-
-            //_laserWeaponSystem = laserWeapon;
-            //_laserWeaponConfig = laserWeaponConfig;
             
             _healthLogic = healthLogic;
             healthLogic.OnNoHealthLeft += DefeatPlayer;
@@ -92,22 +73,6 @@ namespace Player.Logic
             _movementLogic.Configure(_playerCollisionHandler, _playerPhysics, _playerPresentation);
             _weaponsLogic.Configure(_playerPresentation);
             
-            //_playerCollisionHandler.OnRicochetCalled += _playerPhysics.ApplyRicochet;
-            //_playerCollisionHandler.OnDamageReceived += TakeDamage;
-            
-            //_playerEngineParticles = _playerPresentation.EngineParticles;
-            //_playerEngineParticles.Stop();
-            
-            //_invulnerabilityLogic.OnInvulnerabilityEnded += EnableCollisions;
-            
-            //_playerPhysics.SetMovableObject(_playerPresentation, _shipSettings.PlayerMass);
-            
-            // ConfigureBulletWeaponSystem();
-            // ConfigureLaserWeaponSystem();
-            
-            //_invulnerabilityLogic.Configure(_playerSpriteRenderer, _shipSettings.InvulnerabilityDuration);
-            //_uncontrollabilityLogic.Configure(_shipSettings.UncontrollabilityDuration);
-            
             _playerPresentation.gameObject.SetActive(false);
             _movementLogic.StopMovement();
 
@@ -117,11 +82,6 @@ namespace Player.Logic
         public void Tick()
         {
             UpdateUI();
-        }
-
-        public void FixedTick()
-        {
-            //_movementLogic.FixedTick();
         }
 
         public void RotatePlayerWithMouse(Vector2 mouseWorldPosition)
@@ -152,11 +112,6 @@ namespace Player.Logic
         {
             _weaponsLogic.ShootLaser(value);
         }
-
-        // private void EnableCollisions()
-        // {
-        //     _playerCollisionHandler.SetShouldProcessCollisions(true);
-        // }
         
         private void UpdateUI()
         {
@@ -168,26 +123,8 @@ namespace Player.Logic
                 _playerUIModel.SetCurrentSpeed(_playerPhysics.CurrentSpeed);
                 _playerUIModel.SetLaserAmmo(_weaponsLogic.CurrentLaserAmmo);
                 _playerUIModel.SetLaserCooldown(_weaponsLogic.CurrentLaserCooldownProgress);
-            
-                //Zenject bug: Tick is not working in subclasses if there are any bool checks. Calling from here.
-                // _bulletWeaponSystem.Tick();
-                // _laserWeaponSystem.Tick();
             }
         }
-
-        // private void TakeDamage(int damage)
-        // {
-        //     if (_invulnerabilityLogic.IsInvulnerable) return;
-        //     
-        //     _healthSystem.TakeDamage(damage);
-        //     _playerCollisionHandler.SetShouldProcessCollisions(false);
-        //     
-        //     _uncontrollabilityLogic.StartUncontrollabilityPeriod();
-        //     _invulnerabilityLogic.StartInvulnerabilityPeriod();
-        //     
-        //     _playerEngineParticles.Stop();
-        //     _lastParticleToggleTime = Time.time;
-        // }
 
         private void RotatePlayerAtSpeed(Vector2 targetDirection)
         {
@@ -227,68 +164,8 @@ namespace Player.Logic
             _playerPresentation.gameObject.SetActive(true);
         }
 
-        // private void ConfigureBulletWeaponSystem()
-        // {
-        //     _bulletWeaponConfig.Configure(
-        //         projectileType: PoolableObjectType.PlayerBullet,
-        //         firepoints: _playerPresentation.BulletFirepoints,
-        //         projectileSpeed: _weaponsSettings.BulletSpeed,
-        //         projectileDelayedDestruction: false,
-        //         destroyProjectileAfter: 0,
-        //         projectileDamage: _weaponsSettings.BulletDamage,
-        //         projectileAffiliation: EntityAffiliation.Ally,
-        //         projectileDurability: EntityDurability.Fragile,
-        //         shouldSetFirepointAsProjectileParent: false,
-        //         fireRateInterval: _weaponsSettings.BulletFireRateInterval,
-        //         maxAmmo: 0,
-        //         ammoCostPerShot: 0,
-        //         hasInfiniteAmmo: true,
-        //         reloadLength: 0,
-        //         ammoPerReload: 0,
-        //         shouldAutoReloadOnLessThanMaxAmmo: false,
-        //         shouldAutoReloadOnNoAmmo: false,
-        //         shouldDepleteAmmoOnReload: false,
-        //         shouldBlockFireWhileReload: false);
-        //
-        //     _bulletWeaponSystem.Configure(_bulletWeaponConfig);
-        // }
-        //
-        // private void ConfigureLaserWeaponSystem()
-        // {
-        //     _laserWeaponConfig.Configure(
-        //         projectileType: PoolableObjectType.PlayerLaser,
-        //         firepoints: _playerPresentation.LaserFirepoints,
-        //         projectileSpeed: 0,
-        //         projectileDelayedDestruction: true,
-        //         destroyProjectileAfter: _weaponsSettings.LaserDuration,
-        //         projectileDamage: _weaponsSettings.LaserDamage,
-        //         projectileAffiliation: EntityAffiliation.Ally,
-        //         projectileDurability: EntityDurability.Undestructable,
-        //         shouldSetFirepointAsProjectileParent: true,
-        //         fireRateInterval: _weaponsSettings.LaserFireRateInterval,
-        //         maxAmmo: _weaponsSettings.MaxLaserCharges,
-        //         ammoCostPerShot: _weaponsSettings.LaserAmmoPerShot,
-        //         hasInfiniteAmmo: false,
-        //         reloadLength: _weaponsSettings.LaserCooldown,
-        //         ammoPerReload: _weaponsSettings.LaserAmmoPerReload,
-        //         shouldAutoReloadOnLessThanMaxAmmo: true,
-        //         shouldAutoReloadOnNoAmmo: true,
-        //         shouldDepleteAmmoOnReload: false,
-        //         shouldBlockFireWhileReload: false);
-        //
-        //     _laserWeaponSystem.Configure(_laserWeaponConfig);
-        // }
-
         public void Dispose()
         {
-            // if (_playerCollisionHandler != null)
-            // {
-            //     _playerCollisionHandler.OnRicochetCalled -= _playerPhysics.ApplyRicochet;
-            //     _playerCollisionHandler.OnDamageReceived -= TakeDamage;
-            // }
-            
-            // _healthSystem.OnHealthDepleted -= DefeatPlayer;
-            // _invulnerabilityLogic.OnInvulnerabilityEnded -= EnableCollisions;
             _signalBus.Unsubscribe<ResetPlayerSignal>(ResetPlayer);
             _signalBus.Unsubscribe<DisablePlayerSignal>(DisablePlayer);
         }
